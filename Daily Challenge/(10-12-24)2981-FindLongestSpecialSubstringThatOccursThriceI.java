5a680ac07be66dc9d8210e04d68d524e453972ad
Mehraab Singh <singhmehraab32@gmail.com>
2024-12-10 20:58:12 +0530

    Create (10-12-24)2981-FindLongestSpecialSubstringThatOccursThriceI.java

---
+++ Daily Challenge/(10-12-24)2981-FindLongestSpecialSubstringThatOccursThriceI.java
@@ -0,0 +1,33 @@
+class Solution {
+  public int maximumLength(String s) {
+    final int n = s.length();
+    int ans = -1;
+    int runningLen = 0;
+    char prevLetter = '@';
+    int[][] counts = new int[26][n + 1];
+
+    for (final char c : s.toCharArray())
+      if (c == prevLetter) {
+        ++counts[c - 'a'][++runningLen];
+      } else {
+        runningLen = 1;
+        ++counts[c - 'a'][runningLen];
+        prevLetter = c;
+      }
+
+    for (int[] count : counts) {
+      ans = Math.max(ans, getMaxFreq(count, n));
+    }
+
+    return ans;
+  }
+  private int getMaxFreq(int[] count, int maxFreq) {
+    int times = 0;
+    for (int freq = maxFreq; freq >= 1; --freq) {
+      times += count[freq];
+      if (times >= 3)
+        return freq;
+    }
+    return -1;
+  }
+}
