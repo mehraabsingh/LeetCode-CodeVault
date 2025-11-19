b692e31eefda55ffc0004b9e5a1666831730d135
Mehraab Singh <singhmehraab32@gmail.com>
2025-11-19 13:13:12 +0530

    Create (19-11-25)2154-KeepMultiplyingFoundValuesByTwo.java

---
+++ Daily Challenge/(19-11-25)2154-KeepMultiplyingFoundValuesByTwo.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int n : nums) set.add(n);
+        while (set.contains(original)) original *= 2;
+        return original;
+    }
+}
